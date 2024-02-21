76d1a6ab91019e366a1c18000dfd3adf825da756
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-02-21 21:37:01 +0100

    first commit

--- src/main/java/metier/MetierImpl.java
+++ src/main/java/metier/MetierImpl.java
@@ -1,10 +1,8 @@
 package metier;
-
 import dao.IDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
 @Component("metier")
 public class MetierImpl implements IMetier{
     //Couplage faible
